8997961ae5092d5eb3ebe4601e25a827ddf99e5e
Documenter.jl <documenter@juliadocs.github.io>
2022-02-06 01:35:22 +0000

    build based on e03847a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BigCleaner","category":"page"},{"location":"#BigCleaner","page":"Home","title":"BigCleaner","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BigCleaner.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BigCleaner]","category":"page"}]
+}
